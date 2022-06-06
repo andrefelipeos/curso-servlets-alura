b0183d14fcb562e44610f69aa3382a782554dc24
André Sousa <andre.lipe.o@gmail.com>
2022-06-06 11:10:00 -0300

    Transforma o servlet UnicaEntrada em um filtro Controlador e especifica a ordem dos filtros da aplicação no arquivo web.xml.

--- src/main/java/alura/servlets/gerenciador/servlet/ControladorFilter.java
+++ src/main/java/alura/servlets/gerenciador/servlet/ControladorFilter.java
@@ -4,23 +4,28 @@ import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import alura.servlets.gerenciador.acao.Acao;
 
-@WebServlet("/entrada")
-public class UnicaEntradaServlet extends HttpServlet {
+public class ControladorFilter extends HttpFilter implements Filter {
 
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("Filtro Controlador iniciado!");
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+
 		String paramAcao = request.getParameter("acao");
 		String nomeDaClasse = null;
 		String link = null;
@@ -44,6 +49,7 @@ public class UnicaEntradaServlet extends HttpServlet {
 		} else {
 			response.sendRedirect(parametros[1]);
 		}
+		System.out.println("Filtro Controlador chegou ao fim!");
 	}
 
 }
